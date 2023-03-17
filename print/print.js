304b0e466132e39dcb338722aa7662362746dc2d
Niko Brdarski <nikolabrdarski26@gmail.com>
2023-03-17 10:57:01 -0400

    week 7

---
+++ print/print.js
@@ -0,0 +1,20 @@
+
+function askQuestion(question) {
+  
+    const answer = prompt(question);
+
+    return answer;
+  }
+  
+
+  const name = askQuestion("What is your name?");
+  
+
+  console.log(`Hello, ${niko}!`);
+  
+
+  const age = askQuestion("How old are you?");
+  
+  // print the user's answer to the console
+  console.log(`You are ${14} years old.`);
+  
--- week03/Input().js
+++ week03/Input().js
@@ -1,42 +1,25 @@
-prompt.sync
-var correct, guess, problemNumber, quiz;
-quiz = [{
-  "question": "How tall is the worlds tallest tower? ",
-  "choices": ["100ft", "600ft", "2,716ft", "3,143ft"],
-  "answer": "2,716ft"
-}, {
-  "question": "Who is the worlds richest person? ",
-  "choices": ["Bill Gates ", "Elon Musk ", "Jeff Bezos ", "Your Mom"],
-  "answer": "Your Mom"
-}, {
-  "question": "What is the most commenly looked up thing? ",
-  "choices": ["Facebook ", "Youtube ", "Amazon ", "Weather "],
-  "answer": "Facebook"
-}];
-random.shuffle(quiz);
-problemNumber = 0;
-correct = 0;
+answer = input("what is the color of the sky? " )
+if answer == "blue" or answer ==  "Blue":
+    score = score + 1
+    console.log("yes")
+else:
+    console.log("I was looking for blue or Blue")
+answer = input("How old is the president? ")
+if answer == "104":
+    console.log("good")
+    score = score + 1 
+else:
+    console.log("nope")
+answer = input("how many great lakes are there? ")
+if answer == "5":
+    score = score + 1
+    console.log("Oh your smart")
+else:
+    console.log("YOUR AN IDIOT YOU LIVE IN  MICHIGAN THERES 5")
+console.log("your score is ", score)
 
-for (var problem, _pj_c = 0, _pj_a = quiz, _pj_b = _pj_a.length; _pj_c < _pj_b; _pj_c += 1) {
-  problem = _pj_a[_pj_c];
-  problemNumber += 1;
-  console.log(`Question ${problemNumber}: ${problem["question"]}`);
-
-  for (var choice, _pj_f = 0, _pj_d = problem["choices"], _pj_e = _pj_d.length; _pj_f < _pj_e; _pj_f += 1) {
-    choice = _pj_d[_pj_f];
-    console.log(` ${choice}`);
-  }
-
-prompt.sync("Your guess: ");
-
-  if (guess === problem["answer"]) {
-    correct += 1;
-    console.log(`You are correct!`);
-  } else {
-    console.log(`Incorrect`);
-  }
-
-  console.log();
-}
-
-console.log(`Correct: ${correct} of ${problemNumber} (${correct * 100 / problemNumber}&)`);
+answer =input("what kind of gas does a yamaha 125 take, mixed or unleaded? ")
+if answer == "unleaded":
+    console.log("right")
+else:
+  console.log("wrong never fuel a 125 ")
